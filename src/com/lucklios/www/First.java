007c0ac8301edcf3614e492ea5d5eba3bf5ffb2a
unknown <lucklios@qq.com>
2018-01-26 16:39:33 +0800

    first commit

---
+++ src/com/lucklios/www/First.java
@@ -0,0 +1,11 @@
+package com.lucklios.www;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hellow im coming");
+  
+	}
+
+}
